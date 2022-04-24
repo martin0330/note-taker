2945ac161222ddc6f7239af3cb85415101c9148f
martin0330 <martin.cruz0330@yahoo.com>
2022-04-24 17:30:45 -0500

    added and connected routes using use

---
+++ Develop/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require("path"); 
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+
+// Express middleware
+const PORT = process.env.PORT || 3000;;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use Routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.get('/', function(req, res){
+    res.sendFile('./public/index.html')
+})
+
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
+
